1e616950ecc29f50cf157303a45cbbc67267daa3
Lady Vanilla <valanya.one@gmail.com>
2016-06-13 10:26:37 -0500

    Statistics Added

--- Grades/GradeStatistics.cs
+++ Grades/GradeStatistics.cs
@@ -6,5 +6,14 @@ using System.Threading.Tasks;
 
 namespace Grades {
     class GradeStatistics {
+        public GradeStatistics() {
+
+            HighestGrade = 0;
+            LowestGrade = float.MaxValue;
+        }
+
+        public float AverageGrade;
+        public float HighestGrade;
+        public float LowestGrade;
     }
 }
