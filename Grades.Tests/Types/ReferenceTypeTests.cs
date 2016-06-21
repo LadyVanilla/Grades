249924e3ca4708719ecc4b4ac2843bdb92277b78
Lady Vanilla <valanya.one@gmail.com>
2016-06-21 03:47:52 -0500

    Learning about Methods, Fields, Events and Properties.

--- Grades.Tests/Types/ReferenceTypeTests.cs
+++ Grades.Tests/Types/ReferenceTypeTests.cs
@@ -7,7 +7,80 @@ using System.Threading.Tasks;
 
 namespace Grades.Tests.Types {
     [TestClass]
-    public class ReferenceTypeTests {
+    public class TypeTests {
+
+        [TestMethod]
+        public void UsingArrays() {
+
+            float[] grades;
+            grades = new float[3];
+
+            AddGrades(grades);
+
+            Assert.AreEqual(89.1f, grades[1]);
+        }
+
+        private void AddGrades(float[] grades) {
+
+            grades[1] = 89.1f;
+        }
+
+        [TestMethod]
+        public void UpperCaseString() {
+
+            string name = "scott";
+            name = name.ToUpper();
+
+            Assert.AreEqual("SCOTT", name);
+        }
+
+        [TestMethod]
+        public void AddDaysToDateTime() {
+
+            DateTime date = new DateTime(2015, 1, 1);
+            date = date.AddDays(1);
+
+            Assert.AreEqual(2, date.Day);
+        }
+
+        [TestMethod]
+        public void ValueTypesPassByValue() {
+
+            int x = 46;
+            IncrementNumber(x);
+
+            Assert.AreEqual(46, x);
+        }
+
+        private void IncrementNumber(int number) {
+
+            number += 1;
+        }
+
+        [TestMethod]
+        public void ReferenceTypesPassByValue() {
+
+            GradeBook book1 = new GradeBook();
+            GradeBook book2 = book1;
+
+            GiveBookAName(book2);
+            Assert.AreEqual("A Gradebook", book2.Name);
+        }
+
+        private void GiveBookAName(GradeBook book) {
+            
+            book.Name = "A Gradebook";
+        }
+
+        [TestMethod]
+        public void StringComparisons() {
+
+            string name1 = "Scott";
+            string name2 = "Scott";
+
+            bool result = String.Equals(name1, name2, StringComparison.InvariantCultureIgnoreCase);
+            Assert.IsTrue(result);
+        }
 
         [TestMethod]
         public void IntVariablesHoldAValue() {
@@ -24,7 +97,7 @@ namespace Grades.Tests.Types {
 
             GradeBook g1 = new GradeBook();
             GradeBook g2 = g1;
-            
+
             g1.Name = "Scott's grade book";
             Assert.AreEqual(g1.Name, g2.Name);
         }
